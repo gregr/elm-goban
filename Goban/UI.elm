d14586565b3e23ae14230410b67674e2975df5e8
gregr <greg.weiqi@gmail.com>
2016-01-19 05:21:15 -0500

    Factor out defaultStonesForm

--- Goban/UI.elm
+++ Goban/UI.elm
@@ -33,30 +33,37 @@ defaultBoardForm {edgeSize, margin, stoneDiameter} =
       all = GC.group [surface, GC.move (halflen, halflen) <| GC.group [ints, stars]]
   in all
 
+defaultStonesForm : BoardFormSpec -> GP.Board -> GC.Form
+defaultStonesForm {edgeSize, margin, stoneDiameter} =
+  let stoneRad = stoneDiameter / 2
+      scaled x = x * stoneDiameter
+      scaledCoord (x, y) = (scaled <| toFloat x, scaled <| toFloat y)
+      form color = let circle = GC.circle stoneRad
+                       center = GC.filled color circle
+                       border = GC.outlined (GC.solid Color.black) circle
+                   in GC.group [center, border]
+      black = form Color.black
+      white = form Color.white
+      stoneForm st = case st of
+        GP.Black -> black
+        GP.White -> white
+      stoneFormMoved coord = GC.move (scaledCoord coord) << stoneForm
+      ixs = [1 .. edgeSize]
+      halflen = scaled <| -(toFloat edgeSize + margin) / 2
+      stonesForm board =
+        let cell coord = Maybe.map (stoneFormMoved coord) <| GP.get board coord
+            row ypos = List.filterMap (\x -> cell (x, ypos)) ixs
+            stones = GC.group <| List.concat <| List.map row ixs
+        in GC.move (halflen, halflen) stones
+  in stonesForm
 
 
 -- TODO: parametric scaling
 
 boardElement board =
-  let stoneRad = 10
-      scaled x = x * stoneRad * 2
-      sizedCoord (x, y) = (scaled <| toFloat x, scaled <| toFloat y)
-      stoneColor st = case st of
-        GP.Black -> Color.black
-        GP.White -> Color.white
-      stoneForm st = let color = stoneColor st
-                         circle = GC.circle stoneRad
-                         center = GC.filled color circle
-                         border = GC.outlined (GC.solid Color.black) circle
-                     in GC.group [center, border]
-      stoneFormMoved coord = GC.move (sizedCoord coord) << stoneForm
-      ixs = [1 .. board.size]
-      cell coord = Maybe.map (stoneFormMoved coord) <| GP.get board coord
-      row ypos = List.filterMap identity <| List.map (\x -> cell (x, ypos)) ixs
-      stones = GC.group << List.concat <| List.map row ixs
-      bf = defaultBoardForm testBoardFormSpec
-      movedim = scaled <| toFloat (-board.size - 1) / 2
-      all = GC.scale 1.5 <| GC.group [bf, GC.move (movedim, movedim) stones]
+  let bf = defaultBoardForm testBoardFormSpec
+      stones = defaultStonesForm testBoardFormSpec board
+      all = GC.scale 1.5 <| GC.group [bf, stones]
   in GC.collage 400 400 [all]
 
 posToCoord (xa, ya) =
