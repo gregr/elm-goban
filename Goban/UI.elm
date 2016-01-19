dd9573c69a1c84d982289d6e64acffc0b2fdc6b9
gregr <greg.weiqi@gmail.com>
2016-01-19 04:55:43 -0500

    Factor out defaultBoardForm

--- Goban/UI.elm
+++ Goban/UI.elm
@@ -6,58 +6,61 @@ import Goban.Position as GP
 import Color
 import Graphics.Collage as GC
 
+type alias BoardFormSpec = { edgeSize : Int, margin : Float, stoneDiameter : Float }
+testBoardFormSpec = { edgeSize = 9, margin = 1, stoneDiameter = 20 }
+
+defaultBoardForm : BoardFormSpec -> GC.Form
+defaultBoardForm {edgeSize, margin, stoneDiameter} =
+  let edge = toFloat edgeSize
+      scaled x = x * stoneDiameter
+      scaledCoord (x, y) = (scaled x, scaled y)
+      surface = let square = GC.square <| scaled <| edge + margin
+                    center = GC.filled Color.yellow square
+                    border = GC.outlined (GC.solid Color.black) square
+                in GC.group [center, border]
+      line c0 c1 =
+        GC.traced (GC.solid Color.black) <|
+        GC.segment (scaledCoord c0) (scaledCoord c1)
+      hline y = line (1, y) (edge, y)
+      vline x = line (x, 1) (x, edge)
+      ixs = [1..edge]
+      ints = GC.group <| List.map hline ixs ++ List.map vline ixs
+      -- TODO: how should star locations be derived?
+      star = GC.filled (Color.black) <| GC.circle (stoneDiameter / 6)
+      starAt coord = GC.move (scaledCoord coord) star
+      stars = GC.group <| List.map starAt [(3,3), (7,3), (7,7), (3,7), (5,5)]
+      halflen = scaled <| -(edge + margin) / 2
+      all = GC.group [surface, GC.move (halflen, halflen) <| GC.group [ints, stars]]
+  in all
+
+
+
 -- TODO: parametric scaling
-stoneRad = 10
-scaled x = x * stoneRad * 2
-star = GC.filled (Color.black) <| GC.circle (stoneRad / 3)
-
-stoneColor st = case st of
-  GP.Black -> Color.black
-  GP.White -> Color.white
-
-stoneForm st = let color = stoneColor st
-                   circle = GC.circle stoneRad
-                   center = GC.filled color circle
-                   border = GC.outlined (GC.solid Color.black) circle
-               in GC.group [center, border]
-
-sizedCoord (x, y) = (scaled <| toFloat x, scaled <| toFloat y)
-scaledSeg c0 c1 = GC.segment (sizedCoord c0) (sizedCoord c1)
-starAt coord = GC.move (sizedCoord coord) star
-
-stoneFormMoved coord = GC.move (sizedCoord coord) << stoneForm
-
-lineTracer = GC.traced (GC.solid Color.black)
-
-scaledSquare sz = let square = GC.square <| toFloat sz
-                      center = GC.filled Color.yellow square
-                      border = GC.outlined (GC.solid Color.black) square
-                  in GC.group [center, border]
 
 boardElement board =
-  let ixs = [1 .. board.size]
+  let stoneRad = 10
+      scaled x = x * stoneRad * 2
+      sizedCoord (x, y) = (scaled <| toFloat x, scaled <| toFloat y)
+      stoneColor st = case st of
+        GP.Black -> Color.black
+        GP.White -> Color.white
+      stoneForm st = let color = stoneColor st
+                         circle = GC.circle stoneRad
+                         center = GC.filled color circle
+                         border = GC.outlined (GC.solid Color.black) circle
+                     in GC.group [center, border]
+      stoneFormMoved coord = GC.move (sizedCoord coord) << stoneForm
+      ixs = [1 .. board.size]
       cell coord = Maybe.map (stoneFormMoved coord) <| GP.get board coord
       row ypos = List.filterMap identity <| List.map (\x -> cell (x, ypos)) ixs
       stones = GC.group << List.concat <| List.map row ixs
-      offset = stoneRad
-      dim = board.size
-      hline y = lineTracer <| scaledSeg (1, y) (dim, y)
-      vline x = lineTracer <| scaledSeg (x, 1) (x, dim)
-      hints = List.map hline <| ixs
-      vints = List.map vline <| ixs
-      --ints = GC.move (offset, offset) << GC.group <| hints ++ vints
-      -- TODO: how should star locations be derived?
-      backdrop = scaledSquare <| scaled <| board.size + 1
-      stars = GC.group <| List.map starAt [(3,3), (7,3), (7,7), (3,7), (5,5)]
-      ints = GC.group <| hints ++ vints
+      bf = defaultBoardForm testBoardFormSpec
       movedim = scaled <| toFloat (-board.size - 1) / 2
-      all = GC.scale 1.5 <| GC.group [backdrop, GC.move (movedim, movedim) <| GC.group [ints, stars, stones]]
+      all = GC.scale 1.5 <| GC.group [bf, GC.move (movedim, movedim) stones]
   in GC.collage 400 400 [all]
 
--- TODO: coord-clicks signal
-
 posToCoord (xa, ya) =
-  let trans p = round <| toFloat p / scaled 1.5
+  let trans p = round <| toFloat p / 30
       x = trans (xa - 80) + 1
       y = trans (322 - ya) + 1
       legal p = p >= 1 && p <= 9
